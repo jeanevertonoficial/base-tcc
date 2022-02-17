a94d0c232b3c782630f0a2ef89ca85613f11cd33
Jean Everton <sistemas2@leiloesjudiciais.com.br>
2022-02-17 11:27:41 -0300

    finalizado o footer com as categorias

--- public/index.php
+++ public/index.php
@@ -3,3 +3,4 @@
 require_once __DIR__. '/../view/header.phtml';
 require_once __DIR__. '/../view/menu-lateral.phtml';
 require_once __DIR__. '/../view/card-produto.phtml';
+require_once __DIR__. '/../view/FooterBase.phtml';
