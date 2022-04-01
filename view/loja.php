0c91d25003db8d139bc9dc1424208580b616a67b
Jean Everton <sistemas2@leiloesjudiciais.com.br>
2022-04-01 17:56:04 -0300

    criando a sessão do tudo sobre modas

--- view/loja.php
+++ view/loja.php
@@ -7,5 +7,6 @@ require_once __DIR__.' ./../Application/layouts/components/card-produto.phtml';
 require_once __DIR__.' ./../Application/layouts/components/newsletters.phtml';
 require_once __DIR__.' ./../Application/layouts/components/super-destaques.phtml';
 require_once __DIR__.' ./../Application/layouts/components/tudosobremodas.phtml';
+require_once __DIR__.' ./../Application/layouts/components/superpromomodas.phtml';
 require_once __DIR__.' ./../Application/layouts/Botao-topo/botao-topo.phtml';
 require_once __DIR__.' ./../Application/layouts/footer/FooterBase.phtml';
